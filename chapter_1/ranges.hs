9cd1672ab89c675c185294a72db0d64ef9487c2b
Despo Pentara <despo@extractmethod.com>
2013-08-25 16:46:40 +0100

    some ways of creating lists

--- chapter_1/ranges.hs
+++ chapter_1/ranges.hs
@@ -6,3 +6,10 @@ letterRangeFrom start end  = [start..end]
 stepRange step = [ 0, step..100]
 
 reverseRange = [ 20, 19..0]
+
+listOfMultiplesOf x = [x, 2*x..24*x]
+smarterListOfMultiplesOf x = take 24 [x, 2*x..]
+
+repeatList = take 10 (cycle [1,2,3])
+
+replicateThree n = replicate n 3
