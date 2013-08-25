0230c9de7410a05ade19885a8e9a87399a373b65
Despo Pentara <despo@extractmethod.com>
2013-08-25 18:46:48 +0100

    zip

--- chapter_1/tuples.hs
+++ chapter_1/tuples.hs
@@ -5,3 +5,7 @@ second  (_, y, _) =  y
 
 older person1 person2 = if second person1 > second person2  then person1 else person2
 
+productPairs xs ys = zip xs ys
+
+zipFiniteWithInfinite xs = zip [1..] xs
+
