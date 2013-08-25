9b01f90a47c400c14fa00f099391cf55386c5df4
Despo Pentara <despo@extractmethod.com>
2013-08-25 19:03:55 +0100

    1. check triangle validity 2. generate right angle triangles

--- chapter_1/right_triangle.hs
+++ chapter_1/right_triangle.hs
@@ -1,2 +1,4 @@
-validTriangle xs = [(a, b, c) | a <- xs, b <- xs, c <- xs,  c > b, b > a, a+b+c == 24 ]
+validTriangle xs = [(a, b, c) | a <- xs, b <- xs, c <- xs,  c > b, b > a, a+b+c == 24, a^2+b^2 == c^2 ]
 
+
+rightTriangles xs = [ (a,b,c) | c <- xs, a <- [1..c], b <- [1..a], a^2 + b^2 == c^2, a+b+c == 24 ]
