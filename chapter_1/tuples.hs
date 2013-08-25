a5c2d9449e4d0d2cafd417c315890bdf8a797a26
Despo Pentara <despo@extractmethod.com>
2013-08-25 18:23:54 +0100

    tuples, fst and snd only work on pairs the rest, you need to do yourself

---
+++ chapter_1/tuples.hs
@@ -0,0 +1,7 @@
+jenny = ("Jenny Doe", 38, "teacher")
+john = ("John Doe", 40, "musician")
+
+second  (_, y, _) =  y
+
+older person1 person2 = if second person1 > second person2  then person1 else person2
+
