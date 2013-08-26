cc6c754ac341f707cc4dffba276baa4158c37e7e
Despo Pentara <despo@extractmethod.com>
2013-08-26 15:30:19 +0100

    Integer - unbounded, used to store big numbers Int - bounded (-2^63 to 2^63 on a 64bit CPU)

--- chapter_2/examples.hs
+++ chapter_2/examples.hs
@@ -3,3 +3,10 @@ removeNonUpperCase st = [ c | c <- st, c `elem` ['A'..'Z']]
 
 addThree :: Int -> Int -> Int -> Int
 addThree x y z = x + y + z
+
+factorial :: Integer -> Integer
+factorial n = product [1..n]
+
+-- bounded - overflows
+factorial' :: Int -> Int
+factorial' n = product [1..n]
