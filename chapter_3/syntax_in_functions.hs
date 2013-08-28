9c89ed205bc1f43e7ea0b7d11d17459925dcf968
Despo Pentara <despo@extractmethod.com>
2013-08-28 23:43:06 +0100

    pattern matching

---
+++ chapter_3/syntax_in_functions.hs
@@ -0,0 +1,14 @@
+-- pettern matching
+
+lucky :: Int -> String
+lucky 7 = "LUCKY NUMBER 7!"
+lucky x = "Sorry, you're out of luck, pal!"
+
+
+sayMe :: Int -> String
+sayMe 1 = "One!"
+sayMe 2 = "Two!"
+sayMe 3 = "Three!"
+sayMe 4 = "Four!"
+sayMe 5 = "Five!"
+sayMe x = "Not beetween 1 and 5"
